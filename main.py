c58be70e3d466c0f567641492978fa396e0501db
mohammed chaaraoui <shoshomedo876@gmail.com>
2024-11-03 21:12:38 +0100

    update

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request , session , jsonify
-import pexelsPy  , requests , os 
+from flask import Flask, render_template, request, session, jsonify
+import pexelsPy, requests, os
 from tqdm import tqdm
 from dotenv import load_dotenv
 load_dotenv()
 
-
 api_key = os.environ.get('API_KEY')
 api = pexelsPy.API(api_key)
 
-app = Flask(__name__ , template_folder='template')
+app = Flask(__name__, template_folder='template')
+app.secret_key = 'your_secret_key'
 
 @app.route('/')
 def index():
@@ -27,21 +27,25 @@ def download():
         os.mkdir(folder_name)
 
     for i, video in enumerate(videos):
-        url_video = 'https://www.pexels.com/video/' + str(video.id) + '/download'
-        r = requests.get(url_video, stream=True)
-        for data in r.iter_content(block_size):
-            progress_bar.update(len(data))
-            f.write(data)
-            session['progress'] = int(progress_bar.n / total_size_in_bytes * 100)
-            session.modified = True
-        progress_bar.close()
-        with open(os.path.join(folder_name, 'video-' + str(i+1) + '.mp4'), 'wb') as f:
+        # Access the first video file link in the video_files list
+        video_url = video._Video__video['video_files'][0]['link']  # Choose a specific quality if needed
+
+        r = requests.get(video_url, stream=True)
+        with open(os.path.join(folder_name, f'video-{i + 1}.mp4'), 'wb') as f:
             total_size_in_bytes = int(r.headers.get('content-length', 0))
             block_size = 1024
             progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-            for data in r.iter_content(block_size):
-                progress_bar.update(len(data))
-                f.write(data)
+
+            if total_size_in_bytes == 0:
+                for data in r.iter_content(block_size):
+                    f.write(data)
+            else:
+                for data in r.iter_content(block_size):
+                    progress_bar.update(len(data))
+                    f.write(data)
+                    session['progress'] = int(progress_bar.n / total_size_in_bytes * 100)
+                    session.modified = True
+                
             progress_bar.close()
 
     return render_template('download_complete.html')
